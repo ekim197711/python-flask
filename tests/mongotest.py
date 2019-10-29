fc987320a1751c022f9f0810a86e394129e46c33
Mike Møller Nielsen <ekim197711@gmail.com>
2019-10-29 21:47:07 +0100

    pymongo demo

--- tests/mongotest.py
+++ tests/mongotest.py
@@ -1,10 +1,30 @@
 import unittest
-
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        myvar = 2
-        self.assertEqual(2, myvar)
+    def dummyspaceships(self):
+        return [
+            {'name':'Falcon', 'crew': 100},
+            {'name':'Eagle', 'crew': 2500},
+            {'name':'Black bird', 'crew': 42},
+            {'name':'Stork', 'crew': 12},
+            {'name':'Flamingo', 'crew': 5}
+        ]
+
+    def test_createSpaceship(self):
+        client = MongoClient(host="localhost", port=27017)
+        db = client.space
+        spaceships = db.spaceships
+        spaceships.delete_many({})
+        spaceships.insert_many(self.dummyspaceships())
+
+        allspaceships = spaceships.find({'crew': {'$gt': 10}})
+        for ship in allspaceships:
+            print(f'Ship { str(ship) }')
+
+        # onespaceship = spaceships.find_one({'_id': ObjectId('5db8a4cf2728d1296aa37158')})
+        # print('One Ship {}'.format(str(onespaceship)))
 
 
 if __name__ == '__main__':
