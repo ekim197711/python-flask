3b08798551cf32374e37aef99f699d8136303684
Mike Møller Nielsen <ekim197711@gmail.com>
2019-11-02 18:05:45 +0100

    Flask WTF demo - Styling

--- flaskr/SpaceShipForm.py
+++ flaskr/SpaceShipForm.py
@@ -4,9 +4,15 @@ from wtforms.validators import DataRequired, NumberRange, Length
 
 
 class SpaceShipForm(FlaskForm):
-    name = StringField("Name of Spaceship", validators=[DataRequired(), Length(min=5, max=25)])
-    crew = IntegerField("Number of crewmember required", validators=[DataRequired(),
-                                                                     NumberRange(min=10,max=1000)])
-    weight = DecimalField("Weight on Earth", validators=[NumberRange(min=100, max=20000)])
-    submit = SubmitField("Create the spaceship")
+    name = StringField("Name of Spaceship", validators=[DataRequired(), Length(min=5, max=25)],
+                       render_kw={'placeholder': 'Name of the spaceship', 'autocorrect': 'off', 'autocomplete': 'off'})
+    crew = IntegerField("Number of crewmember required",
+                        render_kw={'placeholder': 'Number of crewmembers',
+                                   'autocorrect': 'off', 'autocomplete': 'off'},
+                        validators=[DataRequired(), NumberRange(min=10, max=1000)])
 
+    weight = DecimalField("Weight on Earth",
+                          render_kw={'placeholder': 'Weight of the spaceship',
+                                     'autocorrect': 'off', 'autocomplete': 'off'},
+                          validators=[NumberRange(min=100, max=20000)])
+    submit = SubmitField("Create the spaceship")
